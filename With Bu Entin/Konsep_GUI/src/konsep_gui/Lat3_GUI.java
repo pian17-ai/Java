8b3969739a4e3b212cf9db88f1cf8b9500cfa250
pian17-ai <alviancahyopambudi@gmail.com>
2025-09-10 07:46:57 +0700

    add lat 3

---
+++ With Bu Entin/Konsep_GUI/src/konsep_gui/Lat3_GUI.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package konsep_gui;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author pian
+ */
+public class Lat3_GUI {
+    public static void main(String[] args) {
+//        JFrame jf = new JFrame();
+//        
+//        jf.setTitle("Halo world");
+//        jf.setSize(900, 500);
+//        jf.setResizable(true);
+//        jf.setVisible(true);
+
+          JFrame jf = new JFrame();
+          
+          jf.setTitle("Hello World");
+          jf.setBounds(400, 300, 500, 500);
+          jf.setResizable(true);
+          jf.setVisible(true);
+    }
+}
