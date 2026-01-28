ef4ffe96bbacaba7228224d417847be727ca4f30
pian17-ai <alviancahyopambudi@gmail.com>
2026-01-28 09:30:32 +0700

    add task

---
+++ With Bu Entin/FormCategory/src/formcategory/ConnectionClass.java
@@ -0,0 +1,40 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package formcategory;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author pian
+ */
+public class ConnectionClass {
+    private static java.sql.Connection connDB = null;
+
+    public static java.sql.Connection connectionDB() {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+//            Class.forName("conn.mysql.jdbc.Driver");
+//            String url = "jdbc:mysql://localhost:3306/toko_pian";
+            String url = "jdbc:mysql://localhost:3306/toko_pian?serverTimezone=UTC";
+            String user = "root";
+            String pass = "gr6882899";
+
+            connDB = DriverManager.getConnection(url, user, pass);
+            System.out.println("Connection success");
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println("Connection failed" + e.getMessage());
+        }
+
+        return connDB;
+    }
+
+    public static void main(String[] args) {
+        ConnectionClass kk = new ConnectionClass();
+        kk.connectionDB();
+    }
+}
